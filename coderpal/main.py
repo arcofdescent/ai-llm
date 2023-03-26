74d8620b31ff6ca32970a033eb6621e3fa0ba9fc
Rohan Almeida <rohan.almeida@gmail.com>
2023-03-27 01:47:35 +0530

    format

--- coderpal/main.py
+++ coderpal/main.py
@@ -2,3 +2,15 @@
 This script reads from STDIN, sends related data to the OpenAI API, and prints the output.
 """
 
+import os
+import sys
+from dotenv import load_dotenv
+
+
+def main(args):
+    load_dotenv()
+    openai_api_key = os.getenv("OPENAI_API_KEY")
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
